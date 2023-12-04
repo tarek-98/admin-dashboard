ad95777cdabc5636178e22a527b626de9acff572
Tarek salah <64745962+tarek-98@users.noreply.github.com>
2023-12-04 19:51:38 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      &copy; 2022 All rights reserved by Tarek Salah
+      &copy; 2023 All rights reserved by Tarek Salah
     </p>
   </div>
 );
